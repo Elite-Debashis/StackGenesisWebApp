e6dc4d4218f80c77c54e63d5d3b513f1415ade63
Satyajit Pradhan <satyajiit0@gmail.com>
2019-02-09 23:19:45 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SatyaJit
+ * Date: 09-02-2019
+ * Time: 19:43
+ */
+
+
+//Contains all the values and names,
+
+$WebName="Stack Genesis";
+
+?>
---
+++ header.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SatyaJit
+ * Date: 09-02-2019
+ * Time: 18:40
+ */
+
+echo "<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset=\"utf-8\">
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+  <meta name=\"description\" content=\"Start your development with a Design System for Bootstrap 4.\">
+  <meta name=\"author\" content=\"".$WebName."\">
+  <title>".$WebName."</title>
+  <!-- Favicon -->
+  <link href=\"favicon.png\" rel=\"icon\" type=\"image/png\">
+  <!-- Fonts -->
+  <link href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700\" rel=\"stylesheet\">
+  <!-- Icons -->
+  <link href=\"../assets/vendor/nucleo/css/nucleo.css\" rel=\"stylesheet\">
+  <link href=\"../assets/vendor/font-awesome/css/font-awesome.min.css\" rel=\"stylesheet\">
+  <!-- Argon CSS -->
+  <link type=\"text/css\" href=\"../assets/css/argon.css?v=1.0.1\" rel=\"stylesheet\">
+  <!-- Docs CSS -->
+  <link type=\"text/css\" href=\"../assets/css/docs.min.css\" rel=\"stylesheet\">
+</head>
+
+<body>
+  <header class=\"header-global\">
+    <nav id=\"navbar-main\" class=\"navbar navbar-main navbar-expand-lg navbar-transparent navbar-light headroom\">
+      <div class=\"container\">
+        <a class=\"navbar-brand mr-lg-5\" href=\"#\">
+          <img src=\"../assets/img/brand/white.png\">
+        </a>
+        <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbar_global\" aria-controls=\"navbar_global\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+          <span class=\"navbar-toggler-icon\"></span>
+        </button>
+        <div class=\"navbar-collapse collapse\" id=\"navbar_global\">
+          <div class=\"navbar-collapse-header\">
+            <div class=\"row\">
+              <div class=\"col-6 collapse-brand\">
+                <a href=\"../index.html\">
+                  <img src=\"../assets/img/brand/blue.png\">
+                </a>
+              </div>
+              <div class=\"col-6 collapse-close\">
+                <button type=\"button\" class=\"navbar-toggler\" data-toggle=\"collapse\" data-target=\"#navbar_global\" aria-controls=\"navbar_global\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+                  <span></span>
+                  <span></span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <ul class=\"navbar-nav navbar-nav-hover align-items-lg-center\">
+            <li class=\"nav-item dropdown\">
+              <a href=\"#\" class=\"nav-link\" data-toggle=\"dropdown\" href=\"#\" role=\"button\">
+                <i class=\"ni ni-ui-04 d-lg-none\"></i>
+                <span class=\"nav-link-inner--text\">Technologies</span>
+              </a>
+              <div class=\"dropdown-menu dropdown-menu-xl\">
+                <div class=\"dropdown-menu-inner\">
+                  <a href=\"#\" class=\"media d-flex align-items-center\">
+                    <div class=\"icon icon-shape bg-gradient-primary rounded-circle text-white\">
+                      <i class=\"fa fa-android\"></i>
+                    </div>
+                    <div class=\"media-body ml-3\">
+                      <h6 class=\"heading text-primary mb-md-1\">Android App Development</h6>
+                      <p class=\"description d-none d-md-inline-block mb-0\">Learn how to develop handcrafted apps tailored for day-to-day use and more.</p>
+                    </div>
+                  </a>
+                  <a href=\"#\" class=\"media d-flex align-items-center\">
+                    <div class=\"icon icon-shape bg-gradient-success rounded-circle text-white\">
+                      <i class=\"fa fa-server\"></i>
+                    </div>
+                    <div class=\"media-body ml-3\">
+                      <h6 class=\"heading text-primary mb-md-1\">Web Development</h6>
+                      <p class=\"description d-none d-md-inline-block mb-0\">Learn to build full stack professional websites on the go.</p>
+                    </div>
+                  </a>
+                  <a href=\"#\" class=\"media d-flex align-items-center\">
+                    <div class=\"icon icon-shape bg-gradient-warning rounded-circle text-white\">
+                      <i class=\"fa fa-desktop\"></i>
+                    </div>
+                    <div class=\"media-body ml-3\">
+                      <h5 class=\"heading text-warning mb-md-1\">Desktop App Development</h5>
+                      <p class=\"description d-none d-md-inline-block mb-0\">Build Native Desktop Apps Conveniently while learning with us. </p>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li class=\"nav-item dropdown\">
+              <a href=\"#\" class=\"nav-link\" data-toggle=\"dropdown\" href=\"#\" role=\"button\">
+                <i class=\"ni ni-collection d-lg-none\"></i>
+                <span class=\"nav-link-inner--text\">Projects</span>
+              </a>
+              <div class=\"dropdown-menu\">
+                <a href=\"#\" class=\"dropdown-item\">Ongoing</a>
+                <a href=\"#\" class=\"dropdown-item\">Completed</a>
+              </div>
+            </li>
+            
+            
+             <li class=\"nav-item\">
+              <a href=\"#\" class=\"nav-link\" href=\"#\" role=\"button\">
+                <i class=\"ni ni-collection d-lg-none\"></i>
+                <span class=\"nav-link-inner--text\">Members Panel</span>
+              </a>
+              
+            </li>
+            
+            
+            
+            
+            
+            
+          </ul>
+          <ul class=\"navbar-nav align-items-lg-center ml-lg-auto\">
+            <li class=\"nav-item\">
+              <a class=\"nav-link nav-link-icon\" href=\"#\" target=\"_blank\" data-toggle=\"tooltip\" title=\"Like us on Facebook\">
+                <i class=\"fa fa-facebook-square\"></i>
+                <span class=\"nav-link-inner--text d-lg-none\">Facebook</span>
+              </a>
+            </li>
+            <li class=\"nav-item\">
+              <a class=\"nav-link nav-link-icon\" href=\"#\" target=\"_blank\" data-toggle=\"tooltip\" title=\"Follow us on Instagram\">
+                <i class=\"fa fa-instagram\"></i>
+                <span class=\"nav-link-inner--text d-lg-none\">Instagram</span>
+              </a>
+            </li>
+            <li class=\"nav-item\">
+              <a class=\"nav-link nav-link-icon\" href=\"#\" target=\"_blank\" data-toggle=\"tooltip\" title=\"Follow us on Twitter\">
+                <i class=\"fa fa-twitter-square\"></i>
+                <span class=\"nav-link-inner--text d-lg-none\">Twitter</span>
+              </a>
+            </li>
+            <li class=\"nav-item\">
+              <a class=\"nav-link nav-link-icon\" href=\"#\" target=\"_blank\" data-toggle=\"tooltip\" title=\"Star us on Github\">
+                <i class=\"fa fa-github\"></i>
+                <span class=\"nav-link-inner--text d-lg-none\">Github</span>
+              </a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>";
